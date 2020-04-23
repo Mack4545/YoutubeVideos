fb5ef52d50a3d4690e00ee25f81f9734132e74d3
Mack4545 <seagermack@gmail.com>
2020-04-23 11:55:03 -0400

    Update SortStack.java

--- SortStack.java
+++ SortStack.java
@@ -31,5 +31,43 @@ public class SortStack {
       }
     
   }
+   
+  public class MyStack<T> {
+	
+	private static class StackNode<T> {
+		private T data;
+		private StackNode<T> next;
+		
+		public StackNode(T data) {
+			this.data = data;
+		}
+		
+	}
+	
+	private StackNode<T> top;
+	
+	public T pop() throws Exception {
+		if(top == null) throw new Exception("Empty Stack");
+		T item = top.data;
+		top = top.next;
+		return item;
+	}
+	
+	public void push(T item) {
+		StackNode<T> t = new StackNode<T>(item);
+		t.next = top;
+		top = t;
+	}
+	
+	public T peek() throws Exception {
+		if(top == null) throw new Exception("Empty Stack");
+		return top.data;
+	}
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+   }
 
 }
